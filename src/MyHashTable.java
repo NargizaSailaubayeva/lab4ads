a34b5bda7349646da34afc9203c9eb77831affca
Nargiza <nargizok04s@gmail.com>
2023-05-13 18:59:26 +0500

    contains

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -53,6 +53,17 @@ public class MyHashTable<K, V> {
         }
         return null;
     }
-
+    public boolean contains(V value){
+        for(int i = 0; i<capacity; i++){
+            HashNode node = chainArray[i];
+            while(node!=null){
+                if(node.value.equals(value)){
+                    return true;
+                }
+                node= node.next;
+            }
+        }
+        return false;
+    }
 }
 
