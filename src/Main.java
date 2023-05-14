ab6475416bd407b34e0a7fbbf10359b70a059fb6
Nargiza <nargizok04s@gmail.com>
2023-05-14 16:17:04 +0500

    comments

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
+    //check each classes
     public static void main(String[] args) {
-
         System.out.println("Hello world!");
-
+        MyHashTable nan=new MyHashTable();
+        MyTestingClass ads=new MyTestingClass(null,null);
+        nan.put(45,"as");
+        System.out.println(nan.get(45));
+        ads.test();
+        ads.tester();
     }
 }
--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,8 +1,24 @@
+/**
+ * This is a Java class definition for a generic Hash Table data structure.
+ * The class includes methods for adding, getting, removing, checking if a value exists, getting the key for a value, and counting the number of elements in each bucket.
+ * @param <K> the type of keys in the hash table
+ * @param <V> the type of values in the hash table
+ */
 public class MyHashTable<K, V> {
+    /**
+     * The class has a private inner class HashNode that represents a node in the hash table.
+     * Each node has a key and a value, and a reference to the next node in the chain if there is a collision.
+     */
     private class HashNode<K, V> {
         private K key;
         private V value;
         private HashNode<K, V> next;
+
+        /**
+         * Constructor for HashNode.
+         * @param key the key for the node
+         * @param value the value for the node
+         */
         public HashNode(K key, V value){
             this.key = key;
             this.value = value;
@@ -12,19 +28,33 @@ public class MyHashTable<K, V> {
     private int size;
     private int capacity;
     private HashNode<K,V>[] chainArray;
+    //Constructor for MyHashTable with default capacity of 11.
     public MyHashTable() {
         size = 0;
         capacity = 11;
         chainArray = new HashNode[capacity];
     }
+    //Constructor for MyHashTable with specified capacity.
     public MyHashTable(int M) {
         capacity = M;
         size = 0;
         chainArray = new HashNode[capacity];
     }
+
+    /**
+     * Private hash function that takes the hash code of the key and uses the modulus operator to map it to a bucket in the array.
+     * @param key the key to hash
+     * @return the index of the bucket in the array
+     */
     private int hash(K key) {
         return key.hashCode();
     }
+
+    /**
+     * The put method is used to add a key-value pair to the hash table. If the key already exists, the value is updated.
+     * @param key the key to add or put
+     * @param value te value to add or put
+     */
     public void put(K key, V value) {
         int index = hash(key) % capacity;
         HashNode node = chainArray[index];
@@ -40,6 +70,12 @@ public class MyHashTable<K, V> {
         chainArray[index] = newNode;
         size++;
     }
+
+    /**
+     * The get method is used to get the value associated with a given key.
+     * @param key the key whose associated value is to be returned
+     * @return the value to which the specified key is mapped, or null if this map contains no mapping for the key
+     */
     public V get(K key){
         int index = hash(key);
         HashNode node = chainArray[index];
@@ -53,6 +89,12 @@ public class MyHashTable<K, V> {
         }
         return null;
     }
+
+    /**
+     * The contains method is used to check if a value exists in the hash table.
+     * @param value the value to be searched in the hashtable
+     * @return true if this hashtable contains a mapping for the specified value, false otherwise
+     */
     public boolean contains(V value){
         for(int i = 0; i<capacity; i++){
             HashNode node = chainArray[i];
@@ -65,6 +107,12 @@ public class MyHashTable<K, V> {
         }
         return false;
     }
+
+    /**
+     * The getKey method is used to get the key associated with a given value.
+     * @param value the value to be searched in the hashtable
+     * @return the first key that maps to the specified value, or null if no such mapping exists
+     */
     public K getKey(V value){
         for(int i=0;i<capacity;i++){
             HashNode node= chainArray[i];
@@ -77,6 +125,12 @@ public class MyHashTable<K, V> {
         }
         return null;
     }
+
+    /**
+     * The remove method is used to remove a key-value pair from the hash table.
+     * @param key the key whose mapping is to be removed from the hashtable
+     * @return the value to which the key had been mapped in this hashtable, or null if the key did not have a mapping
+     */
     public V remove(K key){
         int index = hash(key);
         HashNode<K,V> node = chainArray[index];
@@ -99,6 +153,10 @@ public class MyHashTable<K, V> {
         }
         return n;
     }
+
+    /**
+     * The counter method is used to count the number of elements in each bucket of the hash table
+     */
     public void counter() {
         int count = 0;
         for (int i = 0; i < capacity; i++) {
