c37b2c4e7f1b16c4829731d876dc01a65a56296e
Nargiza <nargizok04s@gmail.com>
2023-05-14 15:49:06 +0500

    tester

--- src/MyTestingClass.java
+++ src/MyTestingClass.java
@@ -27,4 +27,19 @@ public class MyTestingClass<K,V> {
         }
         n.counter();
     }
+    public void tester() {
+        MyHashTable nan = new MyHashTable();
+        nan.put(12, "mes");
+        nan.put(1, "kgn");
+        nan.put(25, "sne");
+        nan.put(13, "sae");
+        nan.put(4, "mne");
+        nan.put(49, "nan");
+        nan.put(18, "egnan");
+        System.out.println(nan.getKey("mne"));
+        System.out.println(nan.get(4));
+        System.out.println(nan.contains("egnan"));
+        System.out.println(nan.remove(18));
+        System.out.println(nan.get(49));
+    }
 }
