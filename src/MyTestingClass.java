cf47ca89719b8f430b0ec986306c675b50429341
Nargiza <nargizok04s@gmail.com>
2023-05-14 15:08:58 +0500

    constructors

--- src/MyTestingClass.java
+++ src/MyTestingClass.java
@@ -3,4 +3,9 @@ public class MyTestingClass<K,V> {
     private Random random;
     private K id;
     private V name;
+    public MyTestingClass(K id, V name) {
+        this.id = id;
+        this.name = name;
+        this.random = new Random();
+    }
 }
